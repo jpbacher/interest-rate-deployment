2b64e2588229cf07e65632ba15009764f9f86a65
jpbacher <bacherjp@gmail.com>
2020-07-10 19:26:00 -0400

    removed unused imports

--- packages/lending_model/lending_model/processing/preprocessors.py
+++ packages/lending_model/lending_model/processing/preprocessors.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -21,18 +19,4 @@ class NumericalImputer(BaseEstimator, TransformerMixin):
         X = X.copy()
         for feat in self.features:
             X[feat].fillna(self.imputer_dict[feat], inplace=True)
-        return X
-
-
-class RemoveFeatures(BaseEstimator, TransformerMixin):
-    """Remove unecessary variables."""
-    def __init__(self, features_to_drop=None):
-        self.features = features_to_drop
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X = X.copy()
-        X = X.drop(self.features, axis=1)
         return X
