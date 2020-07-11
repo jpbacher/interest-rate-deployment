f5b46d41060c964d58f5e3402bb8c4fb4baf3242
jpbacher <bacherjp@gmail.com>
2020-07-11 07:54:40 -0400

    added test function

---
+++ packages/lending_model/tests/test_predict.py
@@ -0,0 +1,18 @@
+from packages.lending_model.lending_model.predict import make_prediction
+from packages.lending_model.lending_model.processing.data_management import (
+    load_dataset, split_dataset
+)
+
+
+def test_make_single_prediction():
+    """Make a single prediction on validation data."""
+    # given
+    data = load_dataset(file_name='lending_data.csv')
+    _, X_validation, _, _ = split_dataset(data)
+    single_validation_input = X_validation[0:1]
+    # when
+    subject = make_prediction(input_data=single_validation_input)
+    # then
+    assert subject is not None
+    assert isinstance(subject.get('predictions')[0], float)
+    assert subject > 0
