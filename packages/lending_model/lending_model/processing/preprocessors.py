a63464da5e0e7affc8ebd07d7b89dc769938d81f
jpbacher <bacherjp@gmail.com>
2020-07-12 16:07:33 -0400

    added removal class

--- packages/lending_model/lending_model/processing/preprocessors.py
+++ packages/lending_model/lending_model/processing/preprocessors.py
@@ -20,3 +20,21 @@ class NumericalImputer(BaseEstimator, TransformerMixin):
         for feat in self.features:
             X[feat].fillna(self.imputer_dict[feat], inplace=True)
         return X
+
+
+class RemovePercentageSigns(BaseEstimator, TransformerMixin):
+    """Remove % signs & convert to float data types"""
+    def __init__(self, features=None):
+        if not isinstance(features, list):
+            self.features = [features]
+        else:
+            self.features = features
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feat in self.features:
+           X[feat] = X[feat].str.replace(r'%', '').astype('float')
+        return X
