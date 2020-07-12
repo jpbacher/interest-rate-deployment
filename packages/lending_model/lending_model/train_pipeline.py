e5cb004dc353db432ffbe2057b4c984ccb8d8392
jpbacher <bacherjp@gmail.com>
2020-07-12 18:13:01 -0400

    added clean target to training function

--- packages/lending_model/lending_model/train_pipeline.py
+++ packages/lending_model/lending_model/train_pipeline.py
@@ -1,5 +1,7 @@
 from config import config
-from processing.data_management import load_dataset, split_dataset, save_pipeline
+from processing.data_management import (
+    load_dataset, split_dataset, save_pipeline, clean_target_variable
+)
 import pipeline
 
 
@@ -7,6 +9,8 @@ def run_training():
     """Train the model."""
     # read in data
     data = load_dataset(file_name=config.TRAINING_DATA_FILE)
+    # clean the target variable
+    data = clean_target_variable(data)
     # split training & validation data
     X_train, X_val, y_train, y_val = split_dataset(data)
     print('...fitting model')
