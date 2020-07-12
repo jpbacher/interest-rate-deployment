22e3f6f01618ea8fd13303ba41c7304b89dc67c9
jpbacher <bacherjp@gmail.com>
2020-07-12 09:45:43 -0400

    fixed imports

--- packages/lending_model/lending_model/train_pipeline.py
+++ packages/lending_model/lending_model/train_pipeline.py
@@ -1,6 +1,6 @@
-from .processing.data_management import load_dataset, split_dataset
-from .config import config
-from .pipeline import interest_rate_pipe
+from config import config
+from processing.data_management import load_dataset, split_dataset, save_pipeline
+import pipeline
 
 
 def run_training():
@@ -9,7 +9,10 @@ def run_training():
     data = load_dataset(file_name=config.TRAINING_DATA_FILE)
     # split training & validation data
     X_train, X_val, y_train, y_val = split_dataset(data)
-    interest_rate_pipe.fit(X_train[config.FEATURES], y_train)
+    print('...fitting model')
+    pipeline.interest_rate_pipe.fit(X_train[config.FEATURES], y_train)
+    print('...model fitted & saved')
+    save_pipeline(pipeline.interest_rate_pipe)
 
 
 if __name__ == '__main__':
