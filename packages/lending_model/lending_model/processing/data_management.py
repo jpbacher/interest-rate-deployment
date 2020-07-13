b6d5fd3db2d4050fe25f8ecba4ab67eb9f410df7
jpbacher <bacherjp@gmail.com>
2020-07-13 11:30:55 -0400

    added logging, added function

--- packages/lending_model/lending_model/processing/data_management.py
+++ packages/lending_model/lending_model/processing/data_management.py
@@ -3,6 +3,12 @@ import joblib
 from sklearn.model_selection import train_test_split
 
 from lending_model.config import config
+from lending_model import __version__ as _version
+
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 
 def load_dataset(file_name):
@@ -28,14 +34,25 @@ def split_dataset(data):
 
 def save_pipeline(pipeline_to_persist):
     """Persist the pipeline."""
-    save_file_name = 'rf_model_lending.pkl'
+    save_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
     save_path = config.TRAINED_MODEL_DIR/save_file_name
+    remove_old_pipelines(files_to_keep=save_file_name)
     joblib.dump(pipeline_to_persist, save_path)
-    print('...saved pipeline')
+    _logger.info(f'saved pipeline: {save_file_name}')
 
 
 def load_pipeline(file_name):
     """Load a persisted pipeline."""
     file_path = config.TRAINED_MODEL_DIR/file_name
-    saved_pipeline = joblib.load(file_path)
-    return saved_pipeline
+    trained_pipeline = joblib.load(file_path)
+    return trained_pipeline
+
+
+def remove_old_pipelines(files_to_keep):
+    """Remove old pipelines to ensure a one-to-one mapping between
+    the package version & model version (to be used/imported by other
+    applications.
+    """
+    for model_file in config.TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in [files_to_keep, '__init__.py']:
+            model_file.unlink()
