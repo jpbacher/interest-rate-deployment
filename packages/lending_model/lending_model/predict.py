26803e1bf9682f39283de83e3f58ca519858f587
jpbacher <bacherjp@gmail.com>
2020-07-13 09:32:33 -0400

    added validation function

--- packages/lending_model/lending_model/predict.py
+++ packages/lending_model/lending_model/predict.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from lending_model.processing.data_management import load_pipeline
 from lending_model.config import config
+from lending_model.processing.validation import validate_inputs
 
 
 pipeline_file_name = 'rf_model_lending.pkl'
@@ -11,6 +12,7 @@ _interest_rate_pipe = load_pipeline(pipeline_file_name)
 def make_prediction(input_data):
     """Make prediction using the saved model pipeline."""
     data = pd.read_json(input_data)
-    output = _interest_rate_pipe.predict(data[config.FEATURES])
+    validated_data = validate_inputs(input_data=data)
+    output = _interest_rate_pipe.predict(validated_data[config.FEATURES])
     response = {'predictions': output}
     return response
