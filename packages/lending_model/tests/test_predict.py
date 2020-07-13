b7ab5522a97b81174fad48ff86ce5d4587060982
jpbacher <bacherjp@gmail.com>
2020-07-13 09:34:10 -0400

    added test

--- packages/lending_model/tests/test_predict.py
+++ packages/lending_model/tests/test_predict.py
@@ -1,4 +1,6 @@
-from lending_model.processing.data_management import load_dataset, split_dataset
+from lending_model.processing.data_management import (
+    load_dataset, split_dataset, clean_target_variable
+)
 from lending_model.predict import make_prediction
 
 
@@ -6,6 +8,7 @@ def test_make_single_prediction():
     """Make a single prediction on validation data."""
     # given
     data = load_dataset(file_name='lending_data.csv')
+    data = clean_target_variable(data)
     _, X_validation, _, _ = split_dataset(data)
     single_validation_json = X_validation[0:1].to_json(orient='records')
     # when
@@ -14,3 +17,17 @@ def test_make_single_prediction():
     assert subject is not None
     assert isinstance(subject.get('predictions')[0], float)
     assert subject.get('predictions')[0] > 0
+
+
+def test_make_multiple_predictions():
+    """Make multiple predictions on validation data"""
+    # given
+    data = load_dataset(file_name='lending_data.csv')
+    data = clean_target_variable(data)
+    _, X_validation, _, _ = split_dataset(data)
+    multiple_validation_json = X_validation.to_json(orient='records')
+    # when
+    subject = make_prediction(input_data=multiple_validation_json)
+    # then
+    assert subject is not None
+    assert len(subject.get('predictions')) == 44358
