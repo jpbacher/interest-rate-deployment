190e683d10071d444cce86e179740ffcd43c9805
jpbacher <bacherjp@gmail.com>
2020-07-13 06:42:14 -0400

    assert correction

--- packages/lending_model/tests/test_predict.py
+++ packages/lending_model/tests/test_predict.py
@@ -1,7 +1,5 @@
-from ..lending_model.processing.data_management import (
-    load_dataset, split_dataset
-)
-from ..lending_model.predict import make_prediction
+from lending_model.processing.data_management import load_dataset, split_dataset
+from lending_model.predict import make_prediction
 
 
 def test_make_single_prediction():
@@ -9,10 +7,10 @@ def test_make_single_prediction():
     # given
     data = load_dataset(file_name='lending_data.csv')
     _, X_validation, _, _ = split_dataset(data)
-    single_validation_input = X_validation[0:1]
+    single_validation_json = X_validation[0:1].to_json(orient='records')
     # when
-    subject = make_prediction(input_data=single_validation_input)
+    subject = make_prediction(input_data=single_validation_json)
     # then
     assert subject is not None
     assert isinstance(subject.get('predictions')[0], float)
-    assert subject > 0
+    assert subject.get('predictions')[0] > 0
