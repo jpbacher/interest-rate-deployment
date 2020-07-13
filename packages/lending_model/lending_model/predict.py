dc2ea8bb262af54cfa737f56665824562c4c8884
jpbacher <bacherjp@gmail.com>
2020-07-13 11:32:39 -0400

    added logging

--- packages/lending_model/lending_model/predict.py
+++ packages/lending_model/lending_model/predict.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import logging
 
 from lending_model.processing.data_management import load_pipeline
 from lending_model.config import config
 from lending_model.processing.validation import validate_inputs
+from lending_model import __version__ as _version
 
 
-pipeline_file_name = 'rf_model_lending.pkl'
-_interest_rate_pipe = load_pipeline(pipeline_file_name)
+_logger = logging.getLogger(__name__)
+
+pipeline_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
+_interest_rate_pipe = load_pipeline(file_name=pipeline_file_name)
 
 
 def make_prediction(input_data):
