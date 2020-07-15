005d08ac0d861abcb1126c33326f796a18624051
jpbacher <bacherjp@gmail.com>
2020-07-15 08:15:31 -0400

    added endpoint

--- packages/lending_api/api/controller.py
+++ packages/lending_api/api/controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify
 from lending_model.predict import make_prediction
+from lending_model import __version__ as _version
 
 from api.config import get_logger
+from api import __version__ as api_version
 
 _logger = get_logger(logger_name=__name__)
 
@@ -16,6 +18,12 @@ def health():
         _logger.info('health status OK')
         return 'ok'
 
+
+@prediction_app.route('/version', methods=['GET']):
+def version():
+    if request.method == 'GET':
+        return jsonify({'model_version': _version,
+                        'api_version': api_version})
 
 @prediction_app.route('/v1/predict/lending_rate', methods=['POST'])
 def predict():
