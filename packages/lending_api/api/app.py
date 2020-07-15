52da5954040feec4f9dd1bda56a3627ba9f1110d
jpbacher <bacherjp@gmail.com>
2020-07-15 07:06:48 -0400

    added logging

--- packages/lending_api/api/app.py
+++ packages/lending_api/api/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
+from api.config import get_logger
 
-def create_app():
+_logger = get_logger(logger_name=__name__)
+
+
+def create_app(config_object):
     """Create flask app instance."""
     flask_app = Flask('lending_api')
+    flask_app.config.from_object(config_object)
 
     # import blueprints
     from api.controller import prediction_app
     flask_app.register_blueprint(prediction_app)
+    _logger.debug('Application instance was created')
 
     return flask_app
