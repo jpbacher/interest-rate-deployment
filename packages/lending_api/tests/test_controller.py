43e2eba2a1d2ee98688a24558756839b16f69726
jpbacher <bacherjp@gmail.com>
2020-07-16 08:12:19 -0400

    added test version

--- packages/lending_api/tests/test_controller.py
+++ packages/lending_api/tests/test_controller.py
@@ -4,12 +4,24 @@ from lending_model.config import config as model_config
 from lending_model.processing.data_management import load_dataset, split_dataset
 from lending_model import __version__ as _version
 
+from api import __version__ as api_version
+
 
 def test_health_endpoint_returns_200(flask_test_client):
     # when
     response = flask_test_client.get('/health')
     # then
     assert response.status_code == 200
+
+
+def test_version_endpoint_returns_version(flask_test_client):
+    # when
+    response = flask_test_client.get('/version')
+    # then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    assert response_json['model_version'] == _version
+    assert response_json['api_version'] == api_version
 
 
 def test_prediction_endpoint_returns_prediction(flask_test_client):
