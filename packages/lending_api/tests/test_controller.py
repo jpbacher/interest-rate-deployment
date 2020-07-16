393b787d8eec16717ad2822e03aa8f5596ae9815
jpbacher <bacherjp@gmail.com>
2020-07-16 12:04:06 -0400

    fixed assert statement

--- packages/lending_api/tests/test_controller.py
+++ packages/lending_api/tests/test_controller.py
@@ -1,7 +1,9 @@
 import json
 
 from lending_model.config import config as model_config
-from lending_model.processing.data_management import load_dataset, split_dataset
+from lending_model.processing.data_management import (
+    load_dataset, split_dataset, clean_target_variable
+)
 from lending_model import __version__ as _version
 
 from api import __version__ as api_version
@@ -28,6 +30,7 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     # given
     # load data from lending model package, & get validation set
     data = load_dataset(file_name=model_config.TRAINING_DATA_FILE)
+    data = clean_target_variable(data)
     _, X_validation, _, _ = split_dataset(data)
     post_json = X_validation[0:1].to_json(orient='records')
 
@@ -39,5 +42,5 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     response_json = json.loads(response.data)
     prediction = response_json['predictions']
     response_version = response_json['version']
-    assert prediction > 0
+    assert prediction[0] > 0
     assert response_version == _version
